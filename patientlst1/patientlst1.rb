d5fd35fab204e5bf0fce369cf997bc3e17b5f81e
yusuke mihara <mihara@netlab.jp>
2015-04-27 14:10:38 +0900

    add patientlst1

--- patientlst1/patientlst1.rb
+++ patientlst1/patientlst1.rb
@@ -31,7 +31,7 @@ end
 opt = {
   :host => "sms.orca-ng.org",
   :port => 9201,
-  :path => "/api01rv2/patientgetv2?id=00001",
+  :path => "/api01rv2/patientlst1v2?class=01",
   :ssl  => true,
   :user => "api_mihara3",
   :pass => "5e403a3bbd63e0f98d53befadabaeee76c8ecf3fcd701a80cb0ac9959f9b142b"
@@ -40,7 +40,7 @@ opt = {
 #opt = {
 #  :host => "localhost",
 #  :port => 8000,
-#  :path => "/api01rv2/patientlst2v2?class=01",
+#  :path => "/api01rv2/patientlst1v2?class=01",
 #  :ssl  => false,
 #  :user => "ormaster",
 #  :pass => "ormaster"
@@ -48,13 +48,11 @@ opt = {
 
 data = <<EOF
 <data>
-  <patientlst2req type="record">
-    <Patient_ID_Information type="array">
-      <Patient_ID_Information_child type="record">
-        <Patient_ID type="string">1</Patient_ID>
-      </Patient_ID_Information_child>
-    </Patient_ID_Information>
-  </patientlst2req>
+  <patientlst1req type="record">
+    <Base_StartDate type="string">2015-04-01</Base_StartDate>
+    <Base_EndDate type="string">2015-04-30</Base_EndDate>
+    <Contain_TestPatient_Flag type="string">1</Contain_TestPatient_Flag>
+  </patientlst1req>
 </data>
 EOF
 
@@ -66,21 +64,26 @@ end
 
 # 受け取ったXMLのパース
 root = Crack::XML.parse(xml)
-puts xml
-patient_info = root['xmlio2']['patientinfores']['Patient_Information']
+patient_info = root['xmlio2']['patientlst1res']['Patient_Information']
 pp patient_info
 
 # Thinreportで帳票作成
 
-report = ThinReports::Report.new :layout => 'patient'
-report.start_new_page
+report = ThinReports::Report.create do
+  use_layout 'main', default: true
+  use_layout 'cover', id: :cover
 
-ids = %w|Patient_ID WholeName WholeName_inKana BirthDate Sex|
-ids.each do |id|
-  report.page.item(id.to_sym).value(patient_info[id])
+  start_new_page layout: :cover
+
+  attrs = %w|Patient_ID WholeName WholeName_inKana BirthDate Sex CreateDate UpdateDate TestPatient_Flag|
+  patient_info.each do |p|
+    start_new_page do |page|
+      attrs.each do |a|
+        page.item(a.intern).value(p[a])
+      end
+    end
+  end
+
+  start_new_page layout: 'endcover.tlf'
 end
-
-#report.page.item(:Patient_ID).value(patient_info['Patient_ID'])
-#report.page.item(:WholeName).value(patient_info['WholeName'])
-report.page.item(:image).src('orca.jpg')
-report.generate :filename => 'patient.pdf'
+report.generate :filename => 'patientlst1.pdf'
